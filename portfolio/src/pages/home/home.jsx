136fec989f6e9f14e1afcb8b73dbd0cef592769f
Patrick-jc-Evans <paddy@paddyevans.com>
2025-03-22 00:33:49 +0000

    add temp portfolio image

--- portfolio/src/pages/home/home.jsx
+++ portfolio/src/pages/home/home.jsx
@@ -6,15 +6,17 @@ import anewLogo from "../../assets/anewLogo.svg"
 import plantzoneInfo from "../../projectDetails/plantzone.js"
 import plantzoneLogo from "../../assets/plantzoneLogo.svg"
 import portfolioInfo from "../../projectDetails/portfolio.js"
+import portfolioLogo from "../../assets/githubLogo.svg"
 
 export default function Home() {
     return (
         <body>
             <Header />
             <h1>Projects</h1>
-            <InfoCard projectInfo={anewInfo} imgLocation={anewLogo} />
             <InfoCard projectInfo={plantzoneInfo} imgLocation={plantzoneLogo} />
-            <InfoCard projectInfo={portfolioInfo} />
+            <InfoCard projectInfo={anewInfo} imgLocation={anewLogo} />
+
+            <InfoCard projectInfo={portfolioInfo} imgLocation={portfolioLogo} />
         </body>
     )
 }
--- portfolio/src/projectDetails/portfolio.js
+++ portfolio/src/projectDetails/portfolio.js
@@ -4,5 +4,5 @@ export default {
         "This is a little recursive isn't it? But it would be a shame not to include my portfolio site as part of my portfolio. A far smaller project, I have prioritised making a clean design with accessibility considerations at the front given the lack of time pressure compared to the project completed at NorthCoders. ",
     githubFrontendLink: "https://github.com/patrick-jc-evans/PJCE-Portfolio",
     githubBackendLink: "none",
-    imgBackgroundColor: "#00eeff",
+    imgBackgroundColor: "#202020",
 }
