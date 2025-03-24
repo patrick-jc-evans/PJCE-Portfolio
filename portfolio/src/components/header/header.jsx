1ce522c78865e5185bacd1bab263de276325cc79
Patrick-jc-Evans <paddy@paddyevans.com>
2025-03-24 12:49:05 +0000

    add dynamic styling for switching between mobile and desktop

--- portfolio/src/components/header/header.jsx
+++ portfolio/src/components/header/header.jsx
@@ -3,9 +3,8 @@ import "./header.css"
 export default function Header() {
     return (
         <header>
-            <text id="pjce">Patrick-jc-Evans</text>
-            <br />
-            <text id="dev">Junior JavaScript Developer.</text>
+            <p id="pjce">Patrick-jc-Evans</p>
+            <p id="dev">Junior JavaScript Developer.</p>
         </header>
     )
 }
