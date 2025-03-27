af4591d2f123f77261867f136fdc2ffd0e72f989
Patrick-jc-Evans <paddy@paddyevans.com>
2025-03-27 16:03:37 +0000

    update short descriptions

--- portfolio/src/projectDetails/anew.js
+++ portfolio/src/projectDetails/anew.js
@@ -1,6 +1,7 @@
 export default {
     title: "Anew",
-    shortDescription: "Anew is an imaginary news website...",
+    shortDescription:
+        "Anew is an imaginary news website, showcasing both frontend and backend skills in a site where you can browse/sort articles, leave comments and like content.",
     githubFrontendLink:
         "https://github.com/patrick-jc-evans/PJCE-NC-News-Frontend",
     githubBackendLink: "https://github.com/patrick-jc-evans/PJCE-NC-News",
--- portfolio/src/projectDetails/portfolio.js
+++ portfolio/src/projectDetails/portfolio.js
@@ -1,7 +1,6 @@
 export default {
     title: "Portfolio",
-    shortDescription:
-        "This is a little recursive isn't it? But it would be a shame not to include my portfolio site as part of my portfolio. A far smaller project, I have prioritised making a clean design with accessibility considerations at the front given the lack of time pressure compared to the project completed at NorthCoders. ",
+    shortDescription: "Well this is a bit recursive isn't it?",
     githubFrontendLink: "https://github.com/patrick-jc-evans/PJCE-Portfolio",
     imgBackgroundColor: "#202020",
 }
