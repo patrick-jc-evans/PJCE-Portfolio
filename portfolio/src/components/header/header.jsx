eff1123bcb92354fe49d2ecc819cf53bb15f5dda
Patrick-jc-Evans <paddy@paddyevans.com>
2025-03-28 13:04:52 +0000

    make header text clickable

--- portfolio/src/components/header/header.jsx
+++ portfolio/src/components/header/header.jsx
@@ -3,7 +3,9 @@ import "./header.css"
 export default function Header() {
     return (
         <header>
-            <h1 id="pjce">Patrick-jc-Evans</h1>
+            <a href="/" id="header-link">
+                <h1 id="pjce">Patrick-jc-Evans</h1>
+            </a>
             <h2 id="dev">Junior JavaScript Developer.</h2>
         </header>
     )
